7bba5e9bd1055cb23985b639476b0d0b69bbd4ee
mehmetakifkucukkaya <mehmetakifkucukkaya@stu.ayvansaray.edu.tr>
2022-05-02 14:42:40 +0300

    İlk 2 sayfa oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,191 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Kafe Otomasyon',
+      home: DefaultTabController(
+        length: 2, // 2 Sekmeden oluşacak
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: const [
+                Tab(
+                  text: "MÜŞTERİ",
+                ),
+                Tab(
+                  text: "İŞLETME",
+                ),
+              ],
+            ),
+            centerTitle: true,
+            title: Text('Kafe Otomasyon Sistemi'),
+          ),
+          body: TabBarView(
+            children: [MasaPage(), IsletmePage()],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  //*Masaların Göründüğü Kısım
+  Widget MasaPage() {
+    return SafeArea(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //* MASALAR
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2, // Her sütünda 2 eleman olacak
+                  scrollDirection: Axis.vertical, // Dikey kaydırma yapıyor
+                  shrinkWrap: true,
+                  primary: false,
+                  padding: EdgeInsets.all(5),
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 30,
+                  children: <Widget>[
+                    MasaOlustur(1, "Dolu"),
+                    MasaOlustur(2, "Boş"),
+                    MasaOlustur(3, "Dolu"),
+                    MasaOlustur(4, "Boş"),
+                    MasaOlustur(5, "Dolu"),
+                    MasaOlustur(6, "Dolu"),
+                    MasaOlustur(7, "Dolu"),
+                    MasaOlustur(8, "Dolu"),
+                    MasaOlustur(9, "Dolu"),
+                    MasaOlustur(10, "Dolu"),
+                    MasaOlustur(11, "Dolu"),
+                    MasaOlustur(12, "Dolu"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+  //*Masaları oluşturduğumuz method
+  MasaOlustur(int masaNo, String doluMu) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(top: 25),
+        alignment: Alignment.centerRight,
+        color: Colors.green,
+        width: 125,
+        height: 125,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "Masa $masaNo : $doluMu",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              color: Colors.black,
+              height: 3,
+              width: 175,
             ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.green.shade200),
+                onPressed: () {
+                  print("Masa $masaNo Rezerve Edildi");
+                },
+                child: Text(
+                  "Rezerve Et",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget IsletmePage() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Container(
+              child: CircleAvatar(
+                maxRadius: 50,
+                backgroundColor: Colors.blue.shade300,
+                child: Icon(
+                  Icons.people,
+                  color: Colors.blue,
+                  size: 55,
+                ),
+              ),
+            ),
+          ),
+          Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: "Kullanıcı Adı",
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintText: "Kullanıcı Adını Giriniz",
+                    hintStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: "Şifre",
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintText: "Şifrenizi Giriniz",
+                    hintStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 25),
+          //* Giriş Yap Butonu
+          ElevatedButton(
+            child: Text("GİRİŞ YAP"),
+            onPressed: () {
+              print("Giriş Yapıldı");
+            },
+            
+          )
+        ],
+      ),
     );
   }
 }
