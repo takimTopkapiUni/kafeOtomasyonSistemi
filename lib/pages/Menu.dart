c337784769309ba3da8b755525a5d3690c4d6f02
ismailakcabey <ismailakca399@gmail.com>
2022-05-20 16:31:14 +0300

    menu crud fonksiyonları eklendi

--- lib/pages/Menu.dart
+++ lib/pages/Menu.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Menu extends StatefulWidget {
   const Menu({Key? key}) : super(key: key);
@@ -9,6 +11,7 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   @override
+  late String id,name,price;
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
@@ -19,6 +22,11 @@ class _MenuState extends State<Menu> {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (String Id){
+                      setState(() {
+                        id=Id;
+                      });
+                    },
                     decoration: InputDecoration(
                       labelText: "ID",
                       labelStyle: const TextStyle(color: Colors.black),
@@ -29,6 +37,11 @@ class _MenuState extends State<Menu> {
                   ),
                   const SizedBox(height: 30),
                   TextFormField(
+                    onChanged: (String ad){
+                      setState(() {
+                        name=ad;
+                      });
+                    },
                     decoration: InputDecoration(
                       labelText: "Ürün İsmi",
                       labelStyle: const TextStyle(color: Colors.black),
@@ -39,6 +52,11 @@ class _MenuState extends State<Menu> {
                   ),
                   const SizedBox(height: 30),
                   TextFormField(
+                    onChanged: (String fiyat){
+                      setState(() {
+                        price=fiyat;
+                      });
+                    },
                     decoration: InputDecoration(
                       labelText: "Ürün Fiyatı",
                       labelStyle: const TextStyle(color: Colors.black),
@@ -55,15 +73,21 @@ class _MenuState extends State<Menu> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    veriEkle();
+                  },
                   child: const Text("Ürün Ekle"),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    veriSil();
+                  },
                   child: const Text("Ürün Sil"),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    veriGuncelle();
+                  },
                   child: const Text("Ürün Güncelle"),
                 ),
               ],
@@ -72,5 +96,33 @@ class _MenuState extends State<Menu> {
         ),
       ),
     );
+  }
+  void veriEkle(){
+    DocumentReference veriYolu = FirebaseFirestore.instance.collection('menu').doc(id);
+    Map<String, dynamic> urunler = {
+      'id' : id,
+      'product' : name,
+      'price' : price
+    };
+    veriYolu.set(urunler).whenComplete(() => {
+      Fluttertoast.showToast(msg: "ürün eklendi")
+    });
+  }
+  void veriSil(){
+    DocumentReference veriSil = FirebaseFirestore.instance.collection('menu').doc(id);
+    veriSil.delete().whenComplete(() => {
+      Fluttertoast.showToast(msg: "ürün silindi")
+    });
+  }
+  void veriGuncelle(){
+    DocumentReference veriGuncellemeYolu = FirebaseFirestore.instance.collection('menu').doc(id);
+    Map<String, dynamic> guncellenecekVeri = {
+      'id' : id,
+      'product' : name,
+      'price' : price
+    };
+    veriGuncellemeYolu.update(guncellenecekVeri).whenComplete(() => {
+      Fluttertoast.showToast(msg: "ürün güncellendi")
+    });
   }
 }
